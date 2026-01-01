8b93eba13f483254652ebbbec451db59983d7b55
pweigert64 <pweigert64@gmail.com>
2026-01-01 18:01:55 +0100

    Change const to var for configuration variables

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const MAPTILER_KEY = 'ZscPH8pPo7YWg0t6tF5s';
-const SHEETS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1jF7J6Z5u8hbOCka2VFlVSnughkNsAdCfdvigbW4ioJaM1rkMgxxz8FW38Jhq7q32kq23EhFMUOPa/pub?output=csv';
+var MAPTILER_KEY = 'ZscPH8pPo7YWg0t6tF5s';
+var SHEETS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1jF7J6Z5u8hbOCka2VFlVSnughkNsAdCfdvigbW4ioJaM1rkMgxxz8FW38Jhq7q32kq23EhFMUOPa/pub?output=csv';
+
  
