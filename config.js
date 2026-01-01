2ea652376c16a3f821bb413560d9e8f12b53bfac
pweigert64 <pweigert64@gmail.com>
2026-01-01 11:45:52 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+const MAPTILER_KEY = 'ZscPH8pPo7YWg0t6tF5s';
+const SHEETS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1jF7J6Z5u8hbOCka2VFlVSnughkNsAdCfdvigbW4ioJaM1rkMgxxz8FW38Jhq7q32kq23EhFMUOPa/pub?output=csv';
+ 
