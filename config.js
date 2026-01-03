870fe48b3339d91e6e5a0784e4c74eabfecf8362
pweigert64 <pweigert64@gmail.com>
2026-01-03 18:58:10 +0100

    Update SHEETS_CSV_URL for single output

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
 var MAPTILER_KEY = 'ZscPH8pPo7YWg0t6tF5s';
-var SHEETS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1jF7J6Z5u8hbOCka2VFlVSnughkNsAdCfdvigbW4ioJaM1rkMgxxz8FW38Jhq7q32kq23EhFMUOPa/pub?output=csv';
-
- 
+var SHEETS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR1jF7J6Z5u8hbOCka2VFlVSnughkNsAdCfdvigbW4ioJaM1rkMgxxz8FW38Jhq7q32kq23EhFMUOPa/pub?gid=0&single=true&output=csv';
